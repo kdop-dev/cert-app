8771413fb797d160107f82699a4ae197c4e43425
Anderson Santos <adsantos@gmail.com>
2020-09-25 16:57:54 -0300

    clean-up

--- cert-app/start.sh
+++ cert-app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t kdop/cert-app:0.0.5 .
-docker push kdop/cert-app:0.0.5
+#docker push kdop/cert-app:0.0.5
 docker run --rm -p 5000:5000 --name=cert-app kdop/cert-app:0.0.5
